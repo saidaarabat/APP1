5d200fef903e9d03c67a4573115bacee7a49eac0
saidaarabat <said.aarabat@edu.esiee.fr>
2017-09-18 16:07:17 -0400

    Archive de départ

---
+++ APP1/SondageWS/SondageService/PersistenceException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace USherbrooke.ServiceModel.Sondage
+{
+    /// <summary>
+    /// Cette exception est lancée lorsqu'une transaction avec le médium de stockage permanent (base de données, par exemple) a échoué.
+    /// </summary>
+    [Serializable()]
+    class PersistenceException : Exception
+    {
+        public PersistenceException() : base()
+        {
+        }
+        public PersistenceException(string message) : base(message)
+        {
+        }
+        public PersistenceException(string message, Exception inner) : base(message, inner)
+        {
+        }
+
+        // Needed for serialization.
+        protected PersistenceException(SerializationInfo info, StreamingContext context) : base(info, context)
+       {
+       }
+    }
+}
---
+++ APP1/SondageWS/SondageService/Poll.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace USherbrooke.ServiceModel.Sondage
+{
+    /// <summary>
+    /// Cette classe définit un sondage.
+    /// </summary>
+    [DataContract]
+    public class Poll
+    {
+        /// <summary>
+        /// L'identifiant du sondage.
+        /// </summary>
+        [DataMember]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// La description du sondage.
+        /// </summary>
+        [DataMember]
+        public string Description { get; set; }
+    }
+}
