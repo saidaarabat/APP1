5b0e53c33747998ca1bef1f644b5031df680c319
guinus22800 <guinus22800@hotmail.fr>
2017-09-19 14:51:20 -0400

    Exception

---
+++ APP1_Programmation_sécurisée/WebAPI_Sondage/WebAPI_Sondage/Controllers/InvalidIdException.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace USherbrooke.ServiceModel.Sondage
+{
+    /// <summary>
+    /// Exception lancée lorsque l'identifiant fourni à la couche de persistence n'est pas valide.
+    /// </summary>
+    [Serializable()]
+    class InvalidIdException : PersistenceException
+    {
+        public InvalidIdException() 
+            : base()
+        {
+        }
+        public InvalidIdException(string message) 
+            : base(message)
+        {
+        }
+        public InvalidIdException(string message, Exception inner) 
+            : base(message, inner)
+        {
+        }
+
+        public InvalidIdException(string id, string message)
+            : base(message)
+        {
+            Id = id;
+        }
+        public InvalidIdException(string id, string message, Exception inner)
+            : base(message, inner)
+        {
+            Id = id;
+        }
+
+
+        // Needed for serialization.
+        protected InvalidIdException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+
+        /// <summary>
+        /// L'identifiant fautif.
+        /// </summary>
+        public string Id { get; private set; }
+    }
+}
