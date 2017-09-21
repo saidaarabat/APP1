e05b2b7d9c804b98cb2bc4c38f07624adc3c55b7
guinus22800 <guinus22800@hotmail.fr>
2017-09-21 15:57:38 -0400

    post client

--- APP1_Programmation_sécurisée/ClienConsole/ClientConsole/ClientConsole/Program.cs
+++ APP1_Programmation_sécurisée/ClienConsole/ClientConsole/ClientConsole/Program.cs
@@ -11,6 +11,39 @@ namespace WebAPIClient
 		public static void Main(string[] args)
 		{
             GetQuestions().Wait();
+
+            try
+			{
+                string webAddr = "https://localhost:8081/api/values/1/12";
+
+				var httpWebRequest = (HttpWebRequest)WebRequest.Create(webAddr);
+				httpWebRequest.ContentType = "application/json; charset=utf-8";
+				httpWebRequest.Method = "POST";
+
+				X509Certificate Cert = new X509Certificate();
+				Cert.Import(@"localhost.pfx", "password", X509KeyStorageFlags.PersistKeySet);
+
+				using (var streamWriter = new StreamWriter(httpWebRequest.GetRequestStream()))
+				{
+					string json = "{ \"Id\" : 33 , \"Description\" : \"test\" }";
+
+					streamWriter.Write(json);
+					streamWriter.Flush();
+				}
+				var httpResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+				using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+				{
+					var responseText = streamReader.ReadToEnd();
+					Console.WriteLine(responseText);
+
+					//Now you have your response.
+					//or false depending on information in the response     
+				}
+			}
+			catch (WebException ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
 		}
 
 		static async Task GetQuestions()
