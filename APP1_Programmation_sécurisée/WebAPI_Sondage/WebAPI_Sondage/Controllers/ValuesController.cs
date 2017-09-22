a38fbc226b540d7fea9a2f422c922bb3809c50f8
saidaarabat <said.aarabat@edu.esiee.fr>
2017-09-22 17:22:04 -0400

    aaa

--- APP1_Programmation_sécurisée/WebAPI_Sondage/WebAPI_Sondage/Controllers/ValuesController.cs
+++ APP1_Programmation_sécurisée/WebAPI_Sondage/WebAPI_Sondage/Controllers/ValuesController.cs
@@ -9,6 +9,7 @@ using Newtonsoft.Json.Linq;
 using System.Net.Http;
 using System.IO;
 using System.Net;
+using Newtonsoft.Json;
 
 namespace WebAPI_Sondage.Controllers
 {
@@ -25,26 +26,58 @@ namespace WebAPI_Sondage.Controllers
 		}
 
         // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
+        [HttpGet("{idPoll}/{idQuestion}")]
+        public HttpResponseMessage Get(int idPoll, int idQuestion)
         {
+            HttpResponseMessage reponse = new HttpResponseMessage();
+            Boolean validationToken = false;
             Console.WriteLine("Get");
-            ISondageDAO test = new SimpleSondageDAO();
-            PollQuestion question = test.GetNextQuestion(1, id);
-            return question.Text;
-            
+            List<User> users = LoadJson();
+            string token = "eyJpZCI6IjEiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjEifQ.pQYKhDF1yyKOjPvZQEMSc5LAizD3R2VMzr1rbH6ih0I";
+
+            foreach (User user in users){
+                if (user.token.Equals(token)) {
+                    validationToken = true;
+                }
+            }
+
+            if (validationToken.Equals(true)){
+				reponse.StatusCode = HttpStatusCode.OK;
+				ISondageDAO test = new SimpleSondageDAO();
+				PollQuestion question = test.GetNextQuestion(1, idQuestion);
+                string json = JsonConvert.SerializeObject(question);
+                reponse.Content = new StringContent(json);
+            }
+			else
+                reponse.StatusCode = HttpStatusCode.Unauthorized;
+            return reponse;
         }
 
         [HttpPost]
-        public HttpResponseMessage Post([FromBody]Poll reponseQuestion)
+        public User Post([FromBody]PollQuestion reponseQuestion)
         {
             Console.WriteLine("Post");
             HttpResponseMessage reponse = new HttpResponseMessage();
+            List<User> users = LoadJson();
+
+
+
             if (reponseQuestion != null)
                 reponse.StatusCode = HttpStatusCode.OK;
 			else
 				reponse.StatusCode = HttpStatusCode.NotAcceptable;
-            return reponse;
+            return users.First();
 	    }
+
+        public List<User> LoadJson()
+		{
+			using (StreamReader r = new StreamReader("UserToken.json"))
+			{
+				string json = r.ReadToEnd();
+                List<User> items = JsonConvert.DeserializeObject<List<User>>(json);
+                return items;
+            }
+
+		}
     }
 }
---
+++ APP1_Programmation_sécurisée/WebAPI_Sondage/WebAPI_Sondage/Models/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.Serialization;
+
+namespace WebAPI_Sondage.Models
+{
+    [DataContract]
+    public class User
+	{
+		/// <summary>
+		/// L'identifiant du sondage.
+		/// </summary>
+		[DataMember]
+		public int id { get; set; }
+
+		/// <summary>
+		/// La description du sondage.
+		/// </summary>
+		[DataMember]
+		public string token { get; set; }
+	}
+}
