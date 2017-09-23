e04025fba28c9bcbb65270073bae4226c60aefcd
guinus22800 <guinus22800@hotmail.fr>
2017-09-24 00:08:50 +0200

    web api

---
+++ APP1_Programmation_sécurisée/WebAPI_Sondage/WebAPI_Sondage/Controllers/ValidationToken.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using WebAPI_Sondage.Models;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace WebAPI_Sondage.Controllers
+{
+    // Class validation token
+    public class ValidationToken
+    {
+        // Lecture du fichier JSON avec les tokens
+        private List<User> LoadJson()
+        {
+            using (StreamReader r = new StreamReader("UserToken.json"))
+            {
+                string json = r.ReadToEnd();
+                List<User> items = JsonConvert.DeserializeObject<List<User>>(json);
+                return items;
+            }
+
+        }
+
+        // Validation du token true : token ok
+        public Boolean checkToken(string token)
+        {
+
+            // Variable
+            Boolean validationToken = false;
+            
+            // Lecture fichier JSON
+            List<User> users = LoadJson();
+
+            // Parcours liste Users
+            foreach (User user in users)
+            {
+                // Si le token est dans le fichier alors token valide
+                if (user.token.Equals(token))
+                {
+                    validationToken = true;
+                }
+            }
+
+            // return 
+            return validationToken;
+        }
+    }
+}
--- APP1_Programmation_sécurisée/WebAPI_Sondage/WebAPI_Sondage/Controllers/ValuesController.cs
+++ APP1_Programmation_sécurisée/WebAPI_Sondage/WebAPI_Sondage/Controllers/ValuesController.cs
@@ -15,71 +15,68 @@ namespace WebAPI_Sondage.Controllers
 	[RESTAuthorize]
 	public class ValuesController : Controller
     {
-		// GET api/values
-		[HttpGet]
-		public List<Poll> Categories()
-		{
-			ISondageDAO test = new SimpleSondageDAO();
-			List<Poll> questions = test.GetAvailablePolls().ToList();
-			return questions;
-		}
 
-        // GET api/values/5
+        // GET api/values/idPoll/idQuestion
         [HttpGet("{idPoll}/{idQuestion}")]
-        public PollQuestion Get(int idPoll, int idQuestion)
+        public IActionResult Get(int idPoll, int idQuestion)
         {
 
-            Boolean validationToken = false;
+            // Print console 
             Console.WriteLine("Get");
-			ISondageDAO test = new SimpleSondageDAO();
-			List<User> users = LoadJson();
-            PollQuestion defaultResponse = test.GetNextQuestion(0, 0);
 
-            var requete = Request;
-            var token = requete.Headers["Token"].ToString();
+            // Definition des variables
+			ISondageDAO iSondageDAO = new SimpleSondageDAO();
+            ValidationToken validationToken = new ValidationToken();
 
+            // Recuperation du token dans le header
+            var token = Request.Headers["Token"].ToString();
 
-            foreach (User user in users){
-                if (user.token.Equals(token)) {
-                    validationToken = true;
-                }
-            }
-            if (validationToken.Equals(true))
+            // Validation du token
+            Boolean tokenValidation = validationToken.checkToken(token);
+
+            // Si le token est valide : Code : 200 OK et data 
+            if (tokenValidation.Equals(true))
             {
-                PollQuestion question = test.GetNextQuestion(idPoll, idQuestion);
+                PollQuestion question = iSondageDAO.GetNextQuestion(idPoll, idQuestion);
                 string json = JsonConvert.SerializeObject(question);
-
-                return question;
+                return Ok(question);
             }
             else
-                return defaultResponse;
+            {
+                // Sinon non autorisé
+                return Unauthorized();
+            }
+                
         }
 
         [HttpPost]
-        public User Post([FromBody]PollQuestion reponseQuestion)
+        public IActionResult Post([FromBody]PollQuestion reponseQuestion)
         {
             Console.WriteLine("Post");
-            HttpResponseMessage reponse = new HttpResponseMessage();
-            List<User> users = LoadJson();
 
+            // Definition des variables
+            ISondageDAO iSondageDAO = new SimpleSondageDAO();
+            ValidationToken validationToken = new ValidationToken();
 
+            // Recuperation du token dans le header
+            var token = Request.Headers["Token"].ToString();
 
-            if (reponseQuestion != null)
-                reponse.StatusCode = HttpStatusCode.OK;
-			else
-				reponse.StatusCode = HttpStatusCode.NotAcceptable;
-            return users.First();
-	    }
+            // Validation du token
+            Boolean tokenValidation = validationToken.checkToken(token);
 
-        public List<User> LoadJson()
-		{
-			using (StreamReader r = new StreamReader("UserToken.json"))
-			{
-				string json = r.ReadToEnd();
-                List<User> items = JsonConvert.DeserializeObject<List<User>>(json);
-                return items;
+            // Si le token est valide : Code : 200 OK et on enregistre la reponse recu 
+            if (tokenValidation.Equals(true))
+            {
+                iSondageDAO.SaveAnswer(12, reponseQuestion);
+                return Ok();
             }
+            else
+            {
+                // Sinon non autorisé
+                return Unauthorized();
+            }
+        }
 
-		}
+
     }
 }
